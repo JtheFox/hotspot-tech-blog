20c4263a6ee44109f668947f10d0a460f071dd22
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-01 11:48:18 -0400

    refactor

--- config/init.js
+++ config/init.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 const { DB_NAME } = process.env;
 
 (async () => {
-  try {
-    const statement = `CREATE DATABASE IF NOT EXISTS ${DB_NAME}`;
-    const db = await mysql.createConnection('mysql://root@localhost:3306');
-    await db.execute(statement);
-    console.log(`${DB_NAME} created`);
-    process.exit();
-  } catch (err) {
-    console.log(err);
-    process.exit();
-  }
+    try {
+        const statement = `CREATE DATABASE IF NOT EXISTS ${DB_NAME}`;
+        const db = await mysql.createConnection('mysql://root@localhost:3306');
+        await db.execute(statement);
+        console.log(`${DB_NAME} created`);
+        process.exit();
+    } catch (err) {
+        console.log(err);
+        process.exit();
+    }
 })();
