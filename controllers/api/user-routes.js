14205d61b9f110adfde9292c7bc149223473f301
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-02 16:13:51 -0400

    add user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,123 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// GET all users
+router.get('/', async (req, res) => {
+    try {
+        const dbUserData = await User.findAll({
+            attributes: {
+                exclude: ['password']
+            },
+            include: [
+                {
+                    model: Post,
+                    attributes: ['id', 'title', 'post_content', 'created_at']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'created_at'],
+                    include: {
+                        model: Post,
+                        attributes: ['title']
+                    }
+                }
+            ]
+        });
+        if (dbUserData) res.status(200).json(dbUserData);
+        else res.status(404).json({ message: 'No users found' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
+// GET user by id
+router.get('/:id', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            attributes: {
+                exclude: ['password']
+            },
+            where: {
+                id: req.params.id
+            },
+            include: [
+                {
+                    model: Post,
+                    attributes: ['id', 'title', 'post_content', 'created_at']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'created_at'],
+                    include: {
+                        model: Post,
+                        attributes: ['title']
+                    }
+                }
+            ]
+        });
+        if (dbUserData) res.status(200).json(dbUserData);
+        else res.status(404).json({ message: 'No user found with this id' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// CREATE new user
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Login
+router.get('/login', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+        if (!dbUserData) {
+            res.status(400).json({ message: 'Invalid email or password.' });
+            return;
+        }
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Invalid email or password.' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            console.log(
+                'User Logged In',
+                req.session.cookie
+            );
+            res.status(200).json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+// Logout
+router.post('/', async (req, res) => {
+    if (req.session.loggedIn) req.session.destroy(() => res.status(204).end());
+    else res.status(404).end();
+});
 
 mmodule.exports = router;
