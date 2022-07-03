2ea5330487033cf421f0f56d0e338ddc4e822781
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-03 16:40:21 -0400

    add homepage route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,35 @@
 const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
 
-
+// GET all posts for homepage
+router.get('/', async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            attributes: ['id', 'title', 'content', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ]
+        });
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+        console.log(posts)
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
