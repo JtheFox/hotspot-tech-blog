887adbf505b1bda41e81b1bb7ece6a2bc8d0cade
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-03 18:31:20 -0400

    add get post route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -22,4 +22,44 @@ router.get('/', async (req, res) => {
     }
 });
 
+// GET one post
+router.get('/post/:id', async (req, res) => {
+    if (!req.session.loggedIn) res.redirect('/login');
+    else {
+        try {
+            const dbPostData = await Post.findByPk(req.params.id, {
+                include: [
+                    {
+                        model: Comment,
+                        include: {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    },
+                    {
+                        model: User,
+                        attributes: ['username']
+                    }
+                ]
+            });
+            const post = dbPostData.get({ plain: true });
+            res.render('post', { post, loggedIn: req.session.loggedIn });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
+});
+
+
+// login page
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
 module.exports = router;
