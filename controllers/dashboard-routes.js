e84a9d53d5c5f96d4aa7f73421af4ca634d527b4
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-03 19:32:15 -0400

    bug fixes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,21 @@
 const router = require('express').Router();
-
 const { User, Post } = require('../models');
+
+// GET all posts for a user
+router.get('/', async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            attributes: ['id', 'title', 'content', 'created_at'],
+            include: [{
+                model: User,
+                attributes: ['username']
+            }]
+        });
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
