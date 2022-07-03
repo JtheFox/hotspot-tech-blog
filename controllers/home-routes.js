1f503f80ea9e40b02c87a42b8915aed9155c3f1a
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-03 16:41:52 -0400

    remove unused data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,20 +6,10 @@ router.get('/', async (req, res) => {
     try {
         const dbPostData = await Post.findAll({
             attributes: ['id', 'title', 'content', 'created_at'],
-            include: [
-                {
-                    model: Comment,
-                    attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username']
-                    }
-                },
-                {
+            include: [{
                     model: User,
                     attributes: ['username']
-                },
-            ]
+                }]
         });
         const posts = dbPostData.map((post) => post.get({ plain: true }));
         console.log(posts)
