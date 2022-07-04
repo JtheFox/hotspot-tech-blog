2245e02fc764168a8139485de494ce9c233703fb
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-07-03 20:43:08 -0400

    revert dashboard changes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -71,32 +71,4 @@ router.post('/', async (req, res) => {
     }
 });
 
-// UPDATE post by id
-router.put('/:id', async (req, res) => {
-    try {
-        const dbPostData = await Post.update(
-            {
-                title: req.body.title,
-                content: req.body.content,
-            },
-            { where: { id: req.params.id } }
-        );
-        if (dbPostData) res.status(201).json(dbPostData);
-        else res.status(500).json({ message: 'There was an error while updating the post' });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// DELETE post by id
-router.delete('/:id', async (req, res) => {
-    try {
-        const dbPostData = await Post.destroy({ where: { id: req.params.id } });
-        if (dbPostData) res.status(200).json(dbPostData);
-        else res.status(404).json({ message: 'No post found with this id' });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
 module.exports = router;
